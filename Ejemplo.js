b1951517adfabbf7dbeb780fffa150cc72252c46
SteelFenix09 <f_771@outlook.com>
2025-11-26 13:39:30 -0600

    Actualizacion zuli

--- Ejemplo.js
+++ Ejemplo.js
@@ -4,5 +4,9 @@ function sumar(a,b){
 const restars = (a,b)=> {
     return a-b;
 }
+const dividir = (a,b)=>{
+    return a/b;
+}
 console.log(sumar(2,3));
 console.log(restars(8,4));
+console.log(dividir(10,29));
